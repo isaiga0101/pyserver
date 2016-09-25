30db3e422d507b6d31dbed5fdb2d6e97b65ca97c
Isaiah Gayfield <igayfield7@gmail.com>
2016-09-25 15:14:39 -0700

    Add Class for Scanning Directory

--- python/server.py
+++ python/server.py
@@ -1,13 +1,22 @@
 import sys
 from os import walk
 
+class directory(object):
+	def __init__(self, path):
+		self.path = path
+		self.filelist = []
+		self.dirlist = []
+		for (dirpath, dirnames, filenames) in walk(path):
+			self.filelist.extend(filenames)
+			self.dirlist.extend(dirnames)
+			break
+	def refresh(self):
+		for (dirpath, dirnames, filenames) in walk(self.path):
+			self.filelist.extend(filenames)
+			self.dirlist.extend(dirnames)
+			break
+		
+
 #Get folder from User
-mypath = input("Input folder path: ")
-
-# Scan folder
-f = []
-for (dirpath, dirnames, filenames) in walk(mypath):
-	f.extend(filenames)
-	break
-
-print(f)
+dir = directory(input("Input folder path: "))
+print(dir.filelist, dir.dirlist)
