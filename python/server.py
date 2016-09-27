c814741c3c1692a9f2c60bc95e4ab56e216d6378
Isaiah Gayfield <igayfield7@gmail.com>
2016-09-26 21:26:37 -0700

    Start full scan def

--- python/server.py
+++ python/server.py
@@ -2,10 +2,13 @@ import sys
 from os import walk
 
 class directory(object):
+	"Scans directory and makes lists and dictionaries based on Directory"
 	def __init__(self, path):
 		self.path = path
 		self.filelist = []
 		self.dirlist = []
+		self.fullfilepaths = []
+		self.fulldirpaths = []
 		for (dirpath, dirnames, filenames) in walk(path):
 			self.filelist.extend(filenames)
 			self.dirlist.extend(dirnames)
@@ -15,8 +18,21 @@ class directory(object):
 			self.filelist.extend(filenames)
 			self.dirlist.extend(dirnames)
 			break
+			
+	def fullscan(self):
+		nextdirs = []
 		
-
+		for dirs in self.dirlist:
+			self.fulldirpaths.extend(walk(self.path+"/"+dirs))
+		
 #Get folder from User
 dir = directory(input("Input folder path: "))
-print(dir.filelist, dir.dirlist)
+print("Initial Directory Scan")
+for file in dir.filelist:
+	print("Files: ", file)
+
+for folder in dir.dirlist:
+	print("Folders: ", folder)
+
+dir.fullscan()
+print("Test Full Scan: ",dir.fulldirpaths)
